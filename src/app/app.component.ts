10cde60f45b2ce1abd4dc3146353f98ee2a8a270
maitansang <maitansang.200599@gmail.com>
2019-04-14 10:09:34 +0700

    commit lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template:`
+  <h1>Hello{{title}}</h1>
+  <img [src]="image"/>
+  <my-tutorial></my-tutorial>
+  <input type ="text" [value]="welcomeText"/>
+  `, 
+  styles:['h1{color:green}']
+  
 })
 export class AppComponent {
-  title = 'My First Angular App';
-}
+  //title = 'Mai Tấn Sang I love U';
+  public title="TEDU"
+  //property binding
+  public image ="http://lorempixel.com/300/300";
+  public welcomeText="Welcome to Angular 2"
+} 
---
+++ src/app/tutorial.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-tutorial',
+    template:`
+                <h4>h4 TutorialComponent</h4>
+`,
+styles:['h4{color:red;}']
+})
+export class TutorialComponent{
+
+}

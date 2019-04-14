cedb9d50eb16b05fb55c0aa4787b353b9b2e3d9a
maitansang <maitansang.200599@gmail.com>
2019-04-14 21:53:23 +0700

    commit lesson11

--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -1,17 +1,34 @@
 import {Component} from '@angular/core';
+import { preserveWhitespacesDefault } from '@angular/compiler';
 
 @Component({
     selector: 'my-tutorial',
     template:`
     <h2>{{title}}</h2>
-     <input type="text" [(ngModel)] = "fname" />
-     <input type="text" [(ngModel)] = "lname"/>
-     <br>
-     Full name: {{fname}} {{lname}}
-     `
+    <p [ngClass]="{classOne:cone,classTwo:ctwo}">This ngClass apply style</p>
+    <button (click)="taggle()">taggle</button>
+    <p [ngStyle]="{'font-style':style,'font-size':size}">This paragraph will be apply to ngStyle</p>
+    `,
+
+    styles:[
+        `
+        .classOne{
+            color: white;
+        }
+        .classTwo{
+            background-color:black;
+        }
+        `
+    ]
 })
 export class TutorialComponent{
-    public title="This Tutorial conponent";
-  
+    public cone = true;
+    public ctwo=true;
+    public style="italic";
+    public size="30px";
+  taggle(){
+     this.cone = !this.cone;
+     this.ctwo = !this.ctwo;
+  }
 
 }

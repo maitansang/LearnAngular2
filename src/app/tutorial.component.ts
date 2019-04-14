5e9aa955132f05f34e4f46bb3143121cb56a7eae
maitansang <maitansang.200599@gmail.com>
2019-04-14 11:07:24 +0700

    commit lesson8

--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -3,10 +3,15 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'my-tutorial',
     template:`
-                <h4>h4 TutorialComponent</h4>
-`,
-styles:['h4{color:red;}']
+     <h4>h4 TutorialComponent</h4>
+     <h3 [class.redColor]="applyClass">ApplyClass</h3> 
+     <h4 [style.color]="blueColor?'blue':'orange'">Apply style</h4>
+     `,
+    styles:[`.redColor{
+        color: red;
+    }`]
 })
 export class TutorialComponent{
-
+    public applyClass=true;
+    public blueColor =true;
 }

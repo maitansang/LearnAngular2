1c394db9590dbd0789f23a89412744325eacb98a
maitansang <maitansang.200599@gmail.com>
2019-04-14 20:04:15 +0700

    commit lesson10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TutorialComponent } from './tutorial.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent, TutorialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -3,15 +3,15 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'my-tutorial',
     template:`
-     <h4>{{title}}</h4>
-     <button (mouseover)="OnClick($event)">Click me </button>
-     <input type="text" #name />
+    <h2>{{title}}</h2>
+     <input type="text" [(ngModel)] = "fname" />
+     <input type="text" [(ngModel)] = "lname"/>
+     <br>
+     Full name: {{fname}} {{lname}}
      `
 })
 export class TutorialComponent{
     public title="This Tutorial conponent";
   
-    OnClick(value){
-        console.log(value);
-    }
+
 }

704bc01c2b61dc6ac83f378cc7a2bc5c6df7b57e
maitansang <maitansang.200599@gmail.com>
2019-04-16 09:30:55 +0700

    commit lesson15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,15 @@
 import { Component } from '@angular/core';
-import {TutorialComponent} from './tutorial.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  template:`
-  <h1>{{title | uppercase}}</h1>
-  <p> Date today: {{today | date: 'shortDate'| uppercase}}
-  <p>Percent: {{percentNumber| percent}}</p>
-  <p>e (3.1-5): {{e | number: '3.1-5'}}</p>
-  <pre>{{object | json}} </pre>
-  <p> 2 power 10 ={{2 | exponentialStrength:10}}</p>
-  <ul>
-    <li *ngFor="let i of collection | slice:1:3">{{i}}</li>
-  </ul>
-  `,
-  
+ // styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
-  public title = "Mai Tấn Sang";
-  public today= Date.now();
-  public percentNumber = 1.3495;
-  public e: number = 2.718281828459045;
-  object: Object = {foo:'bar',baz: 'qux', nested: {xyz:3 , numbers:[1,2,3,4,5] }};
-  public collection: string[]=['a','b','c','d'];
+  onSubmit( value : any){
+    console.log(value);
+  }
+  
 } 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TutorialComponent } from './tutorial.component';
 import { FormsModule } from '@angular/forms';
-import { ExponentialStrengthPipe} from './exponential-strength.pipe'; 
+
 @NgModule({
   declarations: [
-    AppComponent, TutorialComponent, ExponentialStrengthPipe
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    BrowserModule, FormsModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

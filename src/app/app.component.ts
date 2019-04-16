c0228829d6ce602e389ae70f8b30e8f65f9c8192
maitansang <maitansang.200599@gmail.com>
2019-04-16 21:49:05 +0700

    commit lesson17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,9 @@ import { Component } from '@angular/core';
 
 })
 export class AppComponent {
+  public cities= [{
+    Id:1, Name:"Ha Noi"},{Id:2, Name:"HCM"
+  }]
   onSubmit( value : any){
     console.log(value);
   }
